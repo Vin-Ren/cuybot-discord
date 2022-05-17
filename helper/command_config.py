97d669f62b6f09af0feb0c5321d4e1500e71817c
vin-ren <65110648+vin-ren@users.noreply.github.com>
2022-05-17 19:32:15 +0900

    ConfigFlags and CommandConfig decorators change
    Added ConfigFlags for HasPerms and BotHasPerms.
    
    Changed __slots__ for decorators, now additional_decorators contains all other decorators except for 'Command' decorator.
    
    Fixed bug for if isinstance checks for additional_decorators, where None is passed to decorator initializer.
    
    Decorators are aggregated into one list in get_decorator without exception. They are ordered according to its priority, lowest to highest. There can only be one of each FIRST and LAST priority.
    
    commands.command now wrapped with a prepared decorator.

--- helper/command_config.py
+++ helper/command_config.py
@@ -1,52 +1,66 @@
 from enum import IntFlag
 from discord.ext import commands
 
-from typing import List, Union
+from typing import List, Dict, Union
 
-from .command_prepared_decorator import Cooldown
-from .command_help import list_help_cmd_hashmap
+from command_prepared_decorator import Cooldown, Command, DecoratorPriority, HasPerms, BotHasPerms
+from command_help import list_help_cmd_hashmap
 
 
 class ConfigFlags(IntFlag):
     @classmethod
-    def get_all(cls):
+    def all_enabled(cls):
         return cls(sum(list(cls)))
+    
+    @classmethod
+    def all_disabled(cls):
+        return ~cls.all_enabled()
+
     NAME = 1
     ALIASES = 2
     COOLDOWN = 4
-    
-    NO_NAME = ~NAME
-    NO_ALIASES = ~ALIASES
-    NO_COOLDOWN = ~COOLDOWN
+    HAS_PERMS = 8
+    BOT_HAS_PERMS = 16
 
 
 class CommandConfig:
-    __slots__ = ['name', 'command', 'desc', 'usage', 'aliases', 'cooldown']
+    __slots__ = ['name', 'command', 'desc', 'usage', 'aliases', 'additional_decorators']
     
     _DEFAULTS  = {}
     
-    def __init__(self, name: str, command: str, desc: str, usage: str, aliases: List[str], cooldown: Union[Cooldown, int] = None, flags: ConfigFlags = ConfigFlags.get_all()):
+    def __init__(self, name: str, command: str, desc: str, usage: str, aliases: List[str], 
+                 cooldown: Union[Cooldown, int] = None, 
+                 has_perms: Union[HasPerms, Dict[str, bool]] = None, 
+                 bot_has_perms: Union[BotHasPerms, Dict[str, bool]] = None, 
+                 flags: ConfigFlags = ConfigFlags.all_enabled()):
         self.name = name
         self.command = command
         self.desc = desc
         self.usage = usage
         self.aliases = aliases
-        self.cooldown = cooldown if isinstance(cooldown, (Cooldown, None)) else Cooldown(1, cooldown, commands.BucketType.user)
+        self.additional_decorators = {ConfigFlags.COOLDOWN: cooldown if isinstance(cooldown, (Cooldown, None.__class__)) else Cooldown(1, cooldown, commands.BucketType.user), 
+                                      ConfigFlags.HAS_PERMS: has_perms if isinstance(has_perms, (HasPerms, None.__class__)) else HasPerms(has_perms), 
+                                      ConfigFlags.BOT_HAS_PERMS: bot_has_perms if isinstance(bot_has_perms, (BotHasPerms, None.__class__)) else HasPerms(bot_has_perms)}
         self.flags = flags
 
     def get_decorator(self):
         def decorate(func):
             # Decorators
-            decorators = [self.cooldown if ConfigFlags.COOLDOWN in self.flags else None]
+            decorators = []
+            decorators.append(Command(name=self.name if ConfigFlags.NAME in self.flags else None,
+                                      aliases=self.aliases if ConfigFlags.ALIASES in self.flags else None))
+            decorators.extend([decorator for flag, decorator in self.additional_decorators.items() if flag in self.flags])
+            
+            # Checks for LAST and FIRST priority
+            decorators_priority_list = [_cls._PRIORITY for _cls in decorators]
+            for priority in [DecoratorPriority.LAST, DecoratorPriority.FIRST]:
+                if decorators_priority_list.count(priority) > 1:
+                    raise RuntimeError("Decorators with priority:{} should only be one.".format(priority.name))
             
             # Post decorators addition, remove None, then sort according to priority, lowest first, highest last.
             decorators = sorted([decor for decor in decorators if decor is not None], key=lambda _cls:_cls._PRIORITY)
             
-            # Last applied decorators, if the order matters then the list order matters.
-            last_applied_decorators = [commands.command(name=self.name if ConfigFlags.NAME in self.flags else None, 
-                                                        aliases=self.aliases if ConfigFlags.ALIASES in self.flags else None)]
-            
-            for decorator in decorators+last_applied_decorators:
+            for decorator in decorators:
                 func = decorator(func)
             return func
         return decorate
